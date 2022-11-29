274b232f74b252fe863baadd67e26a1ab9b4b4f0
Hasan Ali <hasan@algorepublic.com>
2022-11-29 11:01:41 +0500

    css fixes builder

--- src/components/PageBuilder/index.tsx
+++ src/components/PageBuilder/index.tsx
@@ -1,62 +1,64 @@
 import GrapesJS from 'grapesjs';
 import React, { useEffect, useState } from 'react';
-import './index.scss';
-// import './CustomGrapes.css';
 import { Eyebrow } from 'payload/components/elements';
 import plugin from './ExperfyPlugin';
+import blocks from 'grapesjs-blocks-basic';
 import { DefaultTemplate, MinimalTemplate } from 'payload/components/templates';
 import { useStepNav } from 'payload/components/hooks';
-
+import './index.scss';
 export interface GrapesjsReactProps {
-  id: HTMLElement['id'];
+  id: HTMLElement[ 'id' ];
 
-  onInit?(editor?: GrapesJS.Editor): void;
+  onInit?( editor?: GrapesJS.Editor ): void;
 
   onDestroy?(): void;
 }
 
-const PageBuilder: React.FC = (props: GrapesjsReactProps) => {
+const PageBuilder: React.FC<GrapesjsReactProps> = ( props: GrapesjsReactProps ) => {
 
-  const [editor, setEditor] = useState<GrapesJS.Editor | null>(null);
+  const [ editor, setEditor ] = useState<GrapesJS.Editor | null>( null );
   const { setStepNav } = useStepNav();
-  
 
-  useEffect(() => { 
-    setStepNav([
+
+  useEffect( () => {
+    setStepNav( [
       {
         label: 'Page Builder',
         url: '/page-builder',
       },
-    ]);
-  }, [setStepNav]);
+    ] );
+  }, [ setStepNav ] );
 
-  useEffect(() => {
-    const editor = GrapesJS.init({
-      container: '#editor',
-      plugins: [plugin],
+  useEffect( () => {
+
+    const editor = GrapesJS.init( {
+      container: '#gjs',
+      plugins: [ plugin, blocks ],
       height: '100%',
-      showOffsets: true,
-      noticeOnUnload: 0,
-      storageManager: false,
+      storageManager: true,
       fromElement: true,
       pluginsOpts: {
         plugin: {
           useCustomTheme: true,
-
-        }
+        },
+        blocks: {
+          blocks: [ 'column1', 'column2', 'column3', 'column3-7', 'text', 'link', 'image', 'video', 'map' ]
+        },
       }
-    });
-
-    setEditor(editor);
-  }, [setEditor]);
+    } );
+    setEditor( editor );
+  }, [ setEditor ] );
 
   return (
-    
-      <div className="main__content">
-        <Eyebrow />
-        <div id="editor"></div>
+    <div className="main__content">
+      <Eyebrow />
+      <div id="gjs" />
+      <div className='left__panel'>
+        <div className='left__panel__header'>
+          <h3>Page Builder</h3>
+        </div>
       </div>
-    
+    </div>
   );
 }
 
