3e0c7d7f01dcb1ff6f6d2734ad18d2977ac7b128
Hasan Ali <hasan@algorepublic.com>
2022-12-22 18:31:32 +0500

    Squashed commit of the following:
    commit 85df6d1443345a6722b348b866b273509a1ef48d
    Merge: 3de6502 e757b15
    Author: Hasan Ali <hasan@algorepublic.com>
    Date:   Thu Dec 22 16:50:04 2022 +0500
    
        Merge branch 'dev/zee_design_changes' of https://dev.azure.com/experfy/iTARP/_git/itarp-career-portal-cms-service into dev/zee_design_changes
    
    commit 3de6502204a512d5921a150d4a4d099b4b048ad7
    Author: Hasan Ali <hasan@algorepublic.com>
    Date:   Thu Dec 22 16:24:40 2022 +0500
    
        new page builder sidebar template menu designs fixes
    
    commit e757b151c96a106bff6b693e249464326b6bfd15
    Merge: 60910d5 3a8cb34
    Author: Hasan Ali <hasan@algorepublic.com>
    Date:   Thu Dec 22 12:14:03 2022 +0500
    
        new page builder page builder index scss  conflict resolve
    
    commit 3a8cb343b52bd5fbb7bb1e19e9ad65a8f95baebe
    Author: Hasan Ali <hasan@algorepublic.com>
    Date:   Wed Dec 21 12:40:28 2022 +0000
    
        Merged PR 4435: Milestone 3 - Beta

--- src/components/PortalIdentity/BasicInformation/index.tsx
+++ src/components/PortalIdentity/BasicInformation/index.tsx
@@ -1,11 +1,11 @@
-import { Box, Grid } from '@mui/material';
-import { Button, Eyebrow } from 'payload/components/elements';
-import { Form } from 'payload/components/forms';
-import { useConfig } from 'payload/components/utilities';
-import React, { useEffect, useState } from 'react';
-import FormSelect from '../../../blocks/FormSelect';
-import FormTip from '../../../blocks/FormTip';
-import TextInput from '../../../blocks/TextInput';
+import { Box, Grid } from "@mui/material";
+import { Button, Eyebrow } from "payload/components/elements";
+import { Form } from "payload/components/forms";
+import { useConfig } from "payload/components/utilities";
+import React, { useEffect, useState } from "react";
+import FormSelect from "../../../blocks/FormSelect";
+import FormTip from "../../../blocks/FormTip";
+import TextInput from "../../../blocks/TextInput";
 
 export default function BasicInformation(props) {
   const { adminPortal, setAdminPortal, propsdata } = props;
@@ -20,31 +20,31 @@ export default function BasicInformation(props) {
     (collection) => collection.slug === userSlug
   );
 
-  const [apiMethod, setApiMethod] = useState('post');
+  const [apiMethod, setApiMethod] = useState("post");
 
   useEffect(() => {
     if (propsdata?.id) {
-      setApiMethod('patch');
+      setApiMethod("patch");
     } else {
-      setApiMethod('post');
+      setApiMethod("post");
     }
   }, [propsdata]);
 
-  const [touched, setTouched] = useState('');
+  const [touched, setTouched] = useState("");
   return (
     <Box sx={{ p: 1 }}>
       <Form
         //@ts-ignore
         method={apiMethod}
         action={`${serverURL}${api}/basic-portal-identity/${
-          propsdata?.id ?? ''
+          propsdata?.id ?? ""
         }`}
       >
         <div className="row">
           <div className="col-md-8">
             <TextInput
-              label={'Portal Name'}
-              path={'career_portal_name'}
+              label={"Portal Name"}
+              path={"career_portal_name"}
               minLength={3}
               required={true}
               display={propsdata?.career_portal_name}
@@ -53,18 +53,18 @@ export default function BasicInformation(props) {
           </div>
 
           <div className="col-md-4">
-            <div className='tip-wrapper'>
-              {touched === 'career_portal_name' && (
-                <FormTip text={'The go-to-market name of the career portal'} />
-              )} 
-            </div>  
+            <div className="tip-wrapper">
+              {touched === "career_portal_name" && (
+                <FormTip text={"The go-to-market name of the career portal"} />
+              )}
+            </div>
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-8">
             <TextInput
-              path={'portal_id'}
+              path={"portal_id"}
               label="Portal ID"
               required={true}
               display={propsdata?.portal_id}
@@ -73,18 +73,18 @@ export default function BasicInformation(props) {
           </div>
 
           <div className="col-md-4">
-            <div className='tip-wrapper'>
-              {touched === 'portal_id' && (
-                <FormTip text={'The read only filed displays the Portal ID'} />
+            <div className="tip-wrapper">
+              {touched === "portal_id" && (
+                <FormTip text={"The read only filed displays the Portal ID"} />
               )}
-            </div>  
+            </div>
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-8">
             <TextInput
-              path={'portal_url'}
+              path={"portal_url"}
               label="Portal URL"
               required={true}
               display={propsdata?.portal_url}
@@ -93,22 +93,22 @@ export default function BasicInformation(props) {
           </div>
 
           <div className="col-md-4">
-            <div className='tip-wrapper'>
-              {touched === 'portal_url' && (
+            <div className="tip-wrapper">
+              {touched === "portal_url" && (
                 <FormTip
                   text={
-                    'Access your career portal using this domain. This is thee single main domain upon which all applications in your external career portal are based. Don’t include “http” or “https” in easily identify the URL'
+                    "Access your career portal using this domain. This is thee single main domain upon which all applications in your external career portal are based. Don’t include “http” or “https” in easily identify the URL"
                   }
                 />
               )}
-            </div>  
+            </div>
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-8">
             <TextInput
-              path={'company_name'}
+              path={"company_name"}
               label="Company Name"
               display={propsdata?.company_name}
               setTouched={setTouched}
@@ -116,26 +116,26 @@ export default function BasicInformation(props) {
           </div>
 
           <div className="col-md-4">
-            <div className='tip-wrapper'>
-              {touched === 'company_name' && (
+            <div className="tip-wrapper">
+              {touched === "company_name" && (
                 <FormTip
                   text={
-                    'The company of your career Portal. This can be a shortened version of Portal.'
+                    "The company of your career Portal. This can be a shortened version of Portal."
                   }
                 />
               )}
-            </div>  
+            </div>
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-8">
             <FormSelect
-              type={'select'}
-              options={['English', 'Spanish']}
+              type={"select"}
+              options={["English", "Spanish"]}
               label="Default Language"
-              name={'default_language'}
-              path={'default_language'}
+              name={"default_language"}
+              path={"default_language"}
               display={propsdata?.default_language}
               defaultValue="English"
               setTouched={setTouched}
@@ -143,49 +143,49 @@ export default function BasicInformation(props) {
           </div>
 
           <div className="col-md-4">
-            <div className='tip-wrapper'>
-              {touched === 'default_language' && (
+            <div className="tip-wrapper">
+              {touched === "default_language" && (
                 <FormTip
                   text={
-                    'The company of your career Portal. This can be a shortened version of Portal.'
+                    "The company of your career Portal. This can be a shortened version of Portal."
                   }
                 />
               )}
-            </div>  
+            </div>
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-8">
             <FormSelect
-              options={['US', 'ES']}
+              options={["US", "ES"]}
               label="Default Locale"
-              name={'default_locale'}
-              path={'default_locale'}
+              name={"default_locale"}
+              path={"default_locale"}
               display={propsdata?.default_locale}
               defaultValue="US"
-              type={'select'}
+              type={"select"}
               setTouched={setTouched}
             />
           </div>
 
           <div className="col-md-4">
-            <div className='tip-wrapper'>
-              {touched === 'default_locale' && (
+            <div className="tip-wrapper">
+              {touched === "default_locale" && (
                 <FormTip
                   text={
-                    'The company of your career Portal. This can be a shortened version of Portal.'
+                    "The company of your career Portal. This can be a shortened version of Portal."
                   }
                 />
               )}
-            </div>  
+            </div>
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-8">
             <TextInput
-              path={'google_id'}
+              path={"google_id"}
               label="Google Manager Tag ID"
               display={propsdata?.google_id}
               setTouched={setTouched}
@@ -193,22 +193,22 @@ export default function BasicInformation(props) {
           </div>
 
           <div className="col-md-4">
-            <div className='tip-wrapper'>
-              {touched === 'google_id' && (
+            <div className="tip-wrapper">
+              {touched === "google_id" && (
                 <FormTip
                   text={
-                    'After you obtain the Google Tag Manager ID from Google and add it in this field, Experfy Studio Incorporates the necessary code in your career portal for you to use Google Tag Manager. This field only enables Google Tag Manager for your Portal; it doesn’t manage the tags or perform any'
+                    "After you obtain the Google Tag Manager ID from Google and add it in this field, Experfy Studio Incorporates the necessary code in your career portal for you to use Google Tag Manager. This field only enables Google Tag Manager for your Portal; it doesn’t manage the tags or perform any"
                   }
                 />
               )}
-            </div>  
+            </div>
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-8">
             <TextInput
-              path={'google_analytics'}
+              path={"google_analytics"}
               label="Google Analytics ID"
               display={propsdata?.google_analytics}
               setTouched={setTouched}
@@ -216,22 +216,22 @@ export default function BasicInformation(props) {
           </div>
 
           <div className="col-md-4">
-            <div className='tip-wrapper'>
-              {touched === 'google_analytics' && (
+            <div className="tip-wrapper">
+              {touched === "google_analytics" && (
                 <FormTip
                   text={
-                    'To connect Google Analytics with your Advanced careers portal, fill in below your tracking ID. It can take up to 24 hours for your metrics to show. A Google Analytics ID is a string like “UA-000000-2”'
+                    "To connect Google Analytics with your Advanced careers portal, fill in below your tracking ID. It can take up to 24 hours for your metrics to show. A Google Analytics ID is a string like “UA-000000-2”"
                   }
                 />
               )}
-            </div>  
+            </div>
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-8">
             <TextInput
-              path={'google_webmaster'}
+              path={"google_webmaster"}
               label="Google Webmaster ID"
               display={propsdata?.google_webmaster}
               setTouched={setTouched}
@@ -239,18 +239,18 @@ export default function BasicInformation(props) {
           </div>
 
           <div className="col-md-4">
-            <div className='tip-wrapper'>
-              {touched === 'google_webmaster' && (
-                <FormTip text={'The Google Webmaster account.'} />
+            <div className="tip-wrapper">
+              {touched === "google_webmaster" && (
+                <FormTip text={"The Google Webmaster account."} />
               )}
-            </div>  
+            </div>
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-8">
             <TextInput
-              path={'bing_webmaster'}
+              path={"bing_webmaster"}
               label="Bing Webmaster ID"
               display={propsdata?.bing_webmaster}
               setTouched={setTouched}
@@ -258,18 +258,18 @@ export default function BasicInformation(props) {
           </div>
 
           <div className="col-md-4">
-            <div className='tip-wrapper'>
-              {touched === 'bing_webmaster' && (
-                <FormTip text={'The Bing Webmaster Tools account.'} />
+            <div className="tip-wrapper">
+              {touched === "bing_webmaster" && (
+                <FormTip text={"The Bing Webmaster Tools account."} />
               )}
-            </div>  
+            </div>
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-8">
             <TextInput
-              path={'tracking_pixel'}
+              path={"tracking_pixel"}
               label="Tracking Pixel"
               display={propsdata?.tracking_pixel}
               setTouched={setTouched}
@@ -277,15 +277,15 @@ export default function BasicInformation(props) {
           </div>
 
           <div className="col-md-4">
-            <div className='tip-wrapper'>
-              {touched === 'tracking_pixel' && (
+            <div className="tip-wrapper">
+              {touched === "tracking_pixel" && (
                 <FormTip
                   text={
-                    'Track your successful candidate conversions by adding a third-party tracking pixel to your career pages. When a candidate successfully applies (or “converts”), a pixel can be used to track the success of your job advertising campaigns. Tracking pixels are available through third party platforms like Indeed and Facebook. A tracking pixel must be an image.'
+                    "Track your successful candidate conversions by adding a third-party tracking pixel to your career pages. When a candidate successfully applies (or “converts”), a pixel can be used to track the success of your job advertising campaigns. Tracking pixels are available through third party platforms like Indeed and Facebook. A tracking pixel must be an image."
                   }
                 />
               )}
-            </div>  
+            </div>
           </div>
         </div>
 
