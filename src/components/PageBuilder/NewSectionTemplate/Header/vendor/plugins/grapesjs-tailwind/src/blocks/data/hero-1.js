f528a1022876a2463866a02a782fff5c052a9646
zeeshan-algo <zeeshan.ashraf@algorepublic.com>
2022-12-29 14:00:09 +0500

    Header Added

---
+++ src/components/PageBuilder/NewSectionTemplate/Header/vendor/plugins/grapesjs-tailwind/src/blocks/data/hero-1.js
@@ -0,0 +1,48 @@
+export const source = `
+<section class="text-gray-600 body-font">
+   <div class="container mx-auto px-24 py-24 text-center" style="width: 70%;
+      margin: auto;">
+      <div class="text-center">
+         <h1 class="title-font sm:text-4xl text-3xl mb-2 font-medium text-gray-900" style="font-size: 42px;">Add your heading title here</h1>
+         <h2 class="" style="font-size: 23px;">The beneﬁts module highlights the positive characteristics and values within your
+            company or business. Add perks, beneﬁts, or milestones.
+         </h2>
+      </div>
+      <div class="container mx-auto flex px-5 py-12 md:flex-row flex-col items-center">
+         <div class="benefit m-3 p-3">
+            <i class="fa fa-star"></i>
+            <h1 class="my-5" style="font-size: 20px;
+               font-weight: 500;
+               color: #444;
+               text-align:center;">Beneﬁt 01</h1>
+            <p style="text-align:left; line-height: 1.7;">Lorem ipsum dolor sit amet. Est
+               porro distinctio eum eius odit ea
+               facere consequuntur.
+            </p>
+         </div>
+         <div class="benefit m-3 p-3">
+            <i class="fa fa-star"></i>
+            <h1 class="my-5" style="font-size: 20px;
+               font-weight: 500;
+               color: #444;
+               text-align:center;">Beneﬁt 02</h1>
+            <p style="text-align:left; line-height: 1.7;">Lorem ipsum dolor sit amet. Est
+               porro distinctio eum eius odit ea
+               facere consequuntur.
+            </p>
+         </div>
+         <div class="benefit m-3 p-3">
+            <i class="fa fa-star"></i>
+            <h1 class="my-5" style="font-size: 20px;
+               font-weight: 500;
+               color: #444;
+               text-align:center;">Beneﬁt 03</h1>
+            <p style="text-align:left; line-height: 1.7;">Lorem ipsum dolor sit amet. Est
+               porro distinctio eum eius odit ea
+               facere consequuntur.
+            </p>
+         </div>
+      </div>
+   </div>
+</section>
+`;
